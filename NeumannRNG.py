8dc220df74e585182ca1cf0532fd9cd1e80f3c34
toadvine <drubbles@gmail.com>
2015-09-18 16:42:17 -0700

    simple logic implementation attempt

--- NeumannRNG.py
+++ NeumannRNG.py
@@ -2,8 +2,17 @@ __author__ = 'benjamin_sanchez'
 
 def main():
     a_count = int(input)
+    if a_count <= 0:
+        return "error"
     num_list = [int(x) for x in input().split()]
-        for x in num_list:
+    for x in num_list:
+        neu_rng(x)
 
-
+def neu_rng(i):
+    i **= 2
+    if len(str(i)) < 8:
+        while len(str(i)) < 8:
+            i = "0" + str(i)
+    i = str(i)
+    i =
 main()
